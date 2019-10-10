9b8b5fb0b6c43f7f7730ae686359a599bbfce86c
CScori <amilanotte1366@gmail.com>
2019-10-10 12:42:51 -0500

    adds reducer setup

--- thrones/src/index.js
+++ thrones/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
 
 import App from './App';
 
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- thrones/src/reducer/index.js
+++ thrones/src/reducer/index.js
@@ -1,6 +1,9 @@
-const initialState = [
-    {
-        data: 
-        ID, 1
-    }
-]
+const initialState = {
+    thrones: []
+}
+
+export const reducer = (state = initialState, action) = {
+    switch(action.type);
+    default:
+        return state
+}
