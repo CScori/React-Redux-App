d12e93ddb670397ed61c93e4ee1de07a0419a1cf
CScori <amilanotte1366@gmail.com>
2019-10-10 12:45:23 -0500

    adds store set up

--- thrones/src/index.js
+++ thrones/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducer'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
 import App from './App';
 
-const store = createStore(rootReducer, applyMiddleware(logger))
+const store = createStore(rootReducer, applyMiddleware(thunk, logger))
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
